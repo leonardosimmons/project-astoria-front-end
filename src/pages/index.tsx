94742073f88c00401e604e080cd494bb27925b77
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-03-09 12:08:46 -0800

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,8 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-const SERVER_URL = process.env.NODE_ENV === 'production' ? 
-  '' : 'http://localhost:4000/'
 
 const Index = () => {
-
-  useEffect(() => 
-  {
-
-    const source = axios.CancelToken.source();
-    axios.get('/api/test')
-      .then(res => console.log(res))
-      .catch(e => console.log(e));
-
-    return () => {
-      source.cancel();
-    }
-  }, []);
 
   return (
     <div className="text-yellow-700">Hello World</div>
