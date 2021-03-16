887c8d65b2c8c97b0e72bb6bc8e2d5942b6e7227
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-03-16 15:10:55 -0700

    added

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,65 @@
+import { Fragment } from "react";
+
+type Props = {
+  parent: string;
+  value?: string;
+  toggle?: boolean;
+  label?: string | JSX.Element | HTMLElement;
+  labelFront?: string | JSX.Element | HTMLElement;
+  labelBack?: string | JSX.Element | HTMLElement;
+  changed?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  clicked?: () => void;
+};
+
+const Input: React.FunctionComponent<Props> = (
+  { 
+    parent,
+    toggle,
+    value,
+    label,
+    labelFront,
+    labelBack,
+    changed, 
+    clicked
+  }
+): JSX.Element => {
+  return (
+      toggle ? 
+      <Fragment>
+        <input 
+          className={`${ parent }__checkbox`} 
+          id={`${ parent }__toggle`} 
+          type="checkbox"
+          value={ value } 
+          onChange={ clicked } />
+        <label className={`${ parent }__checkbox--button`} htmlFor={`${ parent }__toggle`}>
+          <span className={`${ parent }__checkbox--button--icon`}>{ label || ''}</span>
+        </label> 
+      </Fragment>
+      : 
+      <Fragment>
+        {
+          labelFront && 
+          <label className={`${ parent }__label-front`} htmlFor={`${ parent }__input`}>
+            { labelFront }
+          </label>
+        }
+        <input 
+          className={`${ parent }__input`} 
+          id={`${ parent }__input`} 
+          type="text" 
+          autoComplete="off"
+          value={ value }
+          onClick={ clicked }
+          onChange={ changed }/>
+        {
+          labelBack && 
+          <label className={`${ parent }__label-back`} htmlFor={`${ parent }__input`}>
+            { labelBack }
+          </label>
+        }
+      </Fragment>
+  )
+};
+
+export default Input;
