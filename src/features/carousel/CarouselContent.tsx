bfdce5860761a3485b6292470cb3432f3019240d
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-03-18 22:47:45 -0700

    new feature added

---
+++ src/features/carousel/CarouselContent.tsx
@@ -0,0 +1,90 @@
+
+import React from 'react';
+import { CarouselContentContext } from '../../utils/types';
+
+type Props = {
+  width: number;
+  translate: number;
+  transition: number;
+  slideCount: (count: number) => void;
+  dotCount: (dots: number) => void;
+  height?: number;
+};
+
+export enum Base 
+{
+  height = '20',
+  display = 'flex'
+};
+
+const initialContentContext: CarouselContentContext = {
+  transform: '',
+  transition: '',
+  width: '',
+  height: Base.height,
+  display: Base.display
+}
+
+const CarouselContent: React.FunctionComponent<Props> = (
+  { 
+    width,
+    translate,
+    transition,
+    slideCount,
+    dotCount,
+    height = Base.height,
+    children
+  }
+): JSX.Element => {
+  /* -----------------  SLIDE COUNT  ----------------- */ 
+  //! CREATE CUSTOM HOOK useChildrenCount()
+  const getSlideCount = React.useCallback((): void => 
+  {
+    slideCount(React.Children.count(children));
+  }, [ slideCount, children ]);
+  
+  React.useEffect(() =>
+  {
+    getSlideCount();
+
+    return () => {
+      slideCount(0);
+    }
+  }, [ getSlideCount, slideCount ]);
+
+
+  /* -------------------  DOT COUNT  ------------------ */ 
+  React.useEffect(() => {
+    dotCount(React.Children.count(children));
+  }, [ dotCount, children ]);
+ 
+
+  /* --------------------  CONTEXT  -------------------- */ 
+  const [ context, setContext ] = React.useState<CarouselContentContext>(initialContentContext);
+
+  React.useEffect(() => 
+  {
+    const status = {
+      transform: `translateX(-${ translate }px)`,
+      transition: `transform ease-out ${ transition }s`,
+      height: `${ height }rem`,
+      width: `${ width * React.Children.count(children) }px`, // mulitplies width by # of slides
+      display: context.display,
+    };
+    setContext(status);
+
+    return () => {
+      setContext(initialContentContext);
+    }
+  }, [ width, height, translate, transition, context.display, children ]);
+
+
+  /* --------------------  RENDER  -------------------- */ 
+  return (
+    <div style={ context }>
+      { children }
+    </div>
+  );
+};
+
+export default CarouselContent;
---
+++ src/features/carousel/reducer.ts
@@ -0,0 +1,58 @@
+
+import { CarouselContext } from '../../utils/types/types';
+import { NEXT, PREV, FIRST_SLIDE, LAST_SLIDE, SET_WIDTH, SET_SLIDE_COUNT, SET_DOT_COUNT, CarouselActions } from './action-types';
+
+export default function carouselReducer(state: CarouselContext, action: CarouselActions): CarouselContext
+{
+  switch(action.type)
+  {
+    case NEXT:
+      const nextIndex = state.activeIndex;
+      return {
+        ...state,
+        activeIndex: nextIndex + 1,
+        translate: (nextIndex + 1) * state.width
+      };
+    case PREV:
+      const prevIndex = state.activeIndex;
+      return {
+        ...state,
+        activeIndex: prevIndex - 1,
+        translate: (prevIndex - 1) * state.width
+      }; 
+    case FIRST_SLIDE:
+      const slideCount = state.slideCount;
+      return {
+        ...state,
+        activeIndex: slideCount - 1,
+        translate: (slideCount -1) * state.width
+      };
+    case LAST_SLIDE:
+      return {
+        ...state,
+        activeIndex: 0,
+        translate: 0
+      }
+    case SET_WIDTH:
+      const width = action.payload.width;
+      return {
+        ...state,
+        width: width
+      };
+    case SET_SLIDE_COUNT:
+      const slides = action.payload.count;
+      return {
+        ...state,
+        slideCount: slides
+      };
+    case SET_DOT_COUNT:
+      const dots = action.payload.count;
+      return {
+        ...state,
+        dotCount: dots
+      };
+    default:
+      return state;
+  }
+};
+
