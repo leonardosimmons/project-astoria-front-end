9a0cf9575cc7db51d205177573fc89220f2b076c
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-03-21 23:59:06 -0700

    prototype

--- src/components/navbar/components/DesktopBarInfo.tsx
+++ src/components/navbar/components/DesktopBarInfo.tsx
@@ -1,11 +1,22 @@
 
+
+import { cpnt } from "../../../utils/keys/keys";
+import Container from "../../container/Container";
+
 type Props = {
 
 };
 
 const DesktopBarInfo: React.FunctionComponent<Props> = (): JSX.Element => {
   return (
-
+    <Container parent={`${ cpnt.DESKTOP_NAVIGATION }__information`}>
+      {/* .map() <span/> */}
+      {/* nav icon */}
+      <span className={'mx-2'}>{`United States`}</span>
+      <span className={'mx-2'}>{`English`}</span>
+      {/* telephone icon */}
+      <span className={'mx-2'}>{`+1.877.546.9043`}</span>
+    </Container>
   );
 };
 
