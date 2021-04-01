da5e15d3e4d66618def644c714af20adc8bd62d5
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-04-01 15:51:10 -0700

    added

---
+++ src/components/loader/spinner/LoadSpinner.tsx
@@ -0,0 +1,11 @@
+
+const LoadSpinner: React.FunctionComponent = ():JSX.Element => (
+  <div className={'loading-spinner'}>
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default LoadSpinner;
