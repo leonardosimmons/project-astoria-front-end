ae703e8dc877813f8cc48cb9b8284ce33e7f6dfb
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-04-07 10:52:47 -0700

    added

---
+++ src/pages/api/under-construction/static-data.ts
@@ -0,0 +1,21 @@
+
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function underConstructionData(_: NextApiRequest, res: NextApiResponse) 
+{
+  res.status(200).json
+  (
+    {
+      pageTitle: 'Astoria | Coming Soon',
+      text: {
+        heading: 'This Page Is Under Construction...',
+        lineOne: 'We are currently working on a host of new features',
+        lineTwo: 'This page along with many others will be available soon',
+      },
+      btn: {
+        text: 'Back Home',
+        link: '/'
+      }
+    }
+  )
+};
