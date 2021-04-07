a3c5151e8ebd6e013a38200119053a8236a397ee
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-04-07 16:33:27 -0700

    added

---
+++ src/containers/index/sections/Appointment/Appointment.tsx
@@ -0,0 +1,38 @@
+
+import styles from './Appointment.module.scss';
+import ContentBox from '../../../../components/box/ContentBox';
+import Button from '../../../../components/button/Button';
+import Container from '../../../../components/container/Container';
+import TextBox from '../../../../components/text/Text';
+
+type Props = {
+
+};
+
+
+const Appointment: React.FunctionComponent<Props> = ({}): JSX.Element => {
+  return (
+    <div className={ styles.wrapper }>
+      <ContentBox styles={ styles }>
+        <TextBox 
+          textOne={'YOUR PRIVATE EXPERIENCE AWAITS WITH'}
+          textTwo={'AN INSTORE SHOPPING APPOINTMENT'}/>
+        <TextBox 
+          headingOne={'BOOK AN APPOINTMENT'}/>
+        <Container styles={ styles }>
+          <Button 
+            text={'BOOK YOUR PRIVATE APPOINTMENT'}
+            link={'/under-construction'} 
+            styles={ styles }/>
+          <div className={ styles.btnSpacer }></div>
+          <Button 
+            text={'DISCOVER OUR EXCLUSIVE SERVICES'}
+            link={'/under-construction'}  
+            styles={ styles }/>
+        </Container>
+      </ContentBox>
+    </div>
+  );
+};
+
+export default Appointment;
