bd3c7b0643555a20bf0b5998f63876f8756fd448
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-05-29 01:08:41 -0700

    switched button placement

--- src/containers/pages/register/form/RegistrationForm.tsx
+++ src/containers/pages/register/form/RegistrationForm.tsx
@@ -31,8 +31,8 @@ const registrationForm: React.FunctionComponent<Props> = (
       <Input col labelFront={'Password'} type={'password'} styles={ styles } changed={ password }/>
       <Input col labelFront={'Re-enter password'} type={'password'} styles={ styles } changed={ pwCheck }/>
       <div className={`${ styles.btnContainer } relative center`}>
-        <Input type={'submit'} value={'REGISTER'} classes={'relative btn-hoverConfig btn-activeFocus'}/>
         <Input type={'reset'} value={'RESET'} classes={'relative btn-hoverConfig btn-activeFocus'}/>
+        <Input type={'submit'} value={'REGISTER'} classes={'relative btn-hoverConfig btn-activeFocus'}/>
       </div>
     </Form>
   );
