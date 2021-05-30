e8b103995c08a3c47b919e15235a4957f09c0871
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-05-29 23:45:03 -0700

    added password check verification

--- src/helpers/ValidationController.ts
+++ src/helpers/ValidationController.ts
@@ -6,7 +6,6 @@ interface Validation {
   readonly isValidated: boolean;
   readonly error: string;
   lenCheck: (field: string, len: number) => boolean;
-  sort: (a: string, b: string, c: string, d: string, e: string, f: string, g: string, h: string, i: string) => string;
   registrationForm: (un: string, e: string, a: Combinable, pw: string, pwC: string) => boolean;
   signInForm: (un: string, pw: string) => boolean;
 };
@@ -38,27 +37,6 @@ class ValidationController implements Validation
     return field.length > len ? true : false;
   }
 
-  public sort(a = '', b = '', c = '', d = '', e = '', f = '', g = '', h = '', i = ''): string {
-    let fail: string = '';
-    const checkArr: string[] = [ a, b , c ,d ,e , f, g, h, i ];
-
-    checkArr.forEach((check: string) => {
-      if (check !== '') {
-        if (fail !== '') {
-          fail = fail.concat(' | ').concat(check as string);
-        } else {
-            fail = check;
-        }
-
-        if (fail.match(/\s\|\s.*\s\|\s/)) {
-          fail = fail.replace(' | ', '');
-        }
-      }
-    });
-
-    return fail;
-  };
-
   public registrationForm(un: string, e: string, a: Combinable, pw: string, pwC: string): boolean {
     let fail = '';
     fail = fail.concat(this.username(un));
@@ -68,12 +46,17 @@ class ValidationController implements Validation
     fail = fail.concat(this.password(pwC));
 
     if (fail === '') {
-      this.validate();
-      return true;
+      const check = this.pwCheck(pwC, pw);
+
+      if (check) {
+        this.validate();
+        return true;
+      }
+
+      fail = fail.concat('The passwords entered do not match\n');
     }
 
-
-    this._error = this.sort(fail);
+    this._error = fail;
     return false;
   }; 
 
@@ -109,46 +92,50 @@ class ValidationController implements Validation
   };
 
   private firstName(field: string): string {
-    return field === '' ? 'No First Name was entered \n': '';
+    return field === '' ? 'No First Name was entered\n': '';
   };
 
   private lastName(field: string): string {
-    return field === '' ? 'No Last Name was entered \n': '';
+    return field === '' ? 'No Last Name was entered\n': '';
   };
 
-  private username(field: string) {
-    if (field === '') return 'No Username entered. \n';
-    else if (field?.length && field.length < 5) return 'Usename must be at least 5 characters long. \n';
-    else if (preg_match('\W', field)) return 'Username must only contain letters, numbers, _ and -. \n';
+  private username(field: string): string {
+    if (field === '') return 'No Username entered.\n';
+    else if (field?.length && field.length < 5) return 'Usename must be at least 5 characters long.\n';
+    else if (preg_match('\W', field)) return 'Username must only contain letters, numbers, _ and -.\n';
     
     return '';
   };
 
-  private password(field: string) {
-    if (field === '') return 'No Password entered \n';
-    else if (field?.length && field.length < 6) return 'Password must be at least 6 characters long \n';
+  private password(field: string): string {
+    if (field === '') return 'No Password entered\n';
+    else if (field?.length && field.length < 6) return 'Password must be at least 6 characters long\n';
     else if (!preg_match('[a-z]', field) || !preg_match('[A-Z]', field ) || !preg_match('[0-9]', field)) 
-      return 'Passwords require at least (1) uppercase letter, (1) lowercase letter and (1) number \n';
+      return 'Passwords require at least (1) uppercase letter, (1) lowercase letter and (1) number\n';
     
     return '';
   };
 
-  private age(field: Combinable) {
-    if (field === '' || field === 0) return 'No Age was entered \n';
+  private pwCheck(pw: string, pwC: string): boolean {
+    return pwC === pw ? true : false;
+  }
+
+  private age(field: Combinable): string {
+    if (field === '' || field === 0) return 'No Age was entered\n';
     else if (typeof field === 'string') {
       if (parseInt(field) < 21) {
-        return 'You must be at least 21 years of age to enter \n';
+        return 'You must be at least 21 years of age to enter\n';
       }
     }
-    else if (field < 21) return 'You must be at least 21 years of age to enter \n';
+    else if (field < 21) return 'You must be at least 21 years of age to enter\n';
 
     return '';
   }
   
-  private email(field: string) {
-    if (field === '') return 'No Email address was entered \n';
+  private email(field: string): string {
+    if (field === '') return 'No Email address was entered\n';
     else if (!(field?.indexOf('.') > 0 || field?.indexOf('@') > 0) && preg_match('[^a-zA-z0-9_-]', field)) 
-      return 'The entered email address is invalid \n';
+      return 'The entered email address is invalid\n';
 
     return '';
   }
