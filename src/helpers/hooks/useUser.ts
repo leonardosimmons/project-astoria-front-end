37a3977f2a374ae22239c42aeb2a909e0bf45229
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-06-13 12:20:00 -0700

    made type safe

--- src/helpers/hooks/useUser.ts
+++ src/helpers/hooks/useUser.ts
@@ -2,11 +2,11 @@
 import React from 'react';
 import { useAppDispatch, useAppSelector } from './redux';
 import { signOut as signOutSession } from 'next-auth/client';
-import { UserInfo, UserContext } from '../../utils/types';
+import { UserInfo, UserContext, UserData } from '../../utils/types';
 import { setUser, signOutUser, verifyAndSignInUser } from '../../redux-store/user/actions';
 
 
-const guestProfile: Partial<UserContext> = {
+const guestProfile: UserData = {
   id: 123456789,
   info: {
     name: 'guest', 
@@ -17,8 +17,8 @@ const guestProfile: Partial<UserContext> = {
 
 export function useUser() {
   const dispatch = useAppDispatch();
+  const guest = React.useRef<UserData>(guestProfile);
   const user: UserContext = useAppSelector((state) => state.user);
-  const guest = React.useRef<Partial<UserContext>>(guestProfile);
 
   function guestSignIn(): void {
     dispatch(setUser(guest.current.id as number, guest.current.info!));
