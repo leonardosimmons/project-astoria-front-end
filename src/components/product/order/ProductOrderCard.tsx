4d0445a55d8e9ef77a3b0f23b904c30a60404695
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-06-17 03:10:12 -0700

    made desktop responsive

--- src/components/product/order/ProductOrderCard.tsx
+++ src/components/product/order/ProductOrderCard.tsx
@@ -1,16 +1,89 @@
 
-import styles from 'ProductOrderCard.module.scss';
+import React from 'react';
+
+import styles from './ProductOrderCard.module.scss';
+
+import Container from '../../container';
+import ContentBox from '../../box/ContentBox';
+import BaseGrid from '../../grid/Grid';
+import Image from 'next/image';
+import BaseHeading from '../../heading/Heading';
 
 
 type Props = {
-
+  //chosenQuantity: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 };
 
+const test: any = {
+  price: 1250,
+  qty: 2
+}
 
-const productOrderCard: React.FunctionComponent<Props> = (): JSX.Element => {
+const ProductOrderCard: React.FunctionComponent<Props> = (): JSX.Element => {
   return (
-    <div />
+    <Container wrapper styles={styles}>
+      <ContentBox styles={styles}>
+        <BaseGrid even grid={styles.grid}>
+          <div className={`${styles.imgBox} relative`}>
+            <Image 
+              src={'/images/products/men/pants/DenimPants01.jpg'}
+              alt={'product'}
+              layout={'fill'}
+              objectFit={'contain'}
+            />
+          </div>
+          <div className={styles.detailsBox}>
+            <BaseGrid even grid={styles.detailsGrid}>
+              <BaseHeading classes={styles.detailsHeading}>
+                <h2>{'Astoria American Denim Jeans'}</h2>
+                <p>{'Style# 660285 XKBVB 4594'}</p>
+                <p>{'Size: Large'}</p>
+              </BaseHeading>
+              <div className={styles.detailsAvail}>
+                <p>{'AVAILABLE'}</p>
+                <p>{'Your selection is available for immediate purchase online.'}</p>
+              </div>
+              <button>{'REMOVE'}</button>
+            </BaseGrid>
+          </div>
+          <BaseGrid even grid={styles.optionsGrid}>
+            <div className={styles.optionsBox}>
+              <form className={styles.qtyForm}>
+                <select>
+                  <option value={1}>{`QTY: ${test.qty}`}</option>
+                  <option value={1}>{1}</option>
+                  <option value={2}>{2}</option>
+                  <option value={3}>{3}</option>
+                  <option value={4}>{4}</option>
+                  <option value={5}>{5}</option>
+                  <option value={6}>{6}</option>
+                  <option value={7}>{7}</option>
+                  <option value={8}>{8}</option>
+                  <option value={9}>{9}</option>
+                  <option value={10}>{10}</option>
+                  <option value={11}>{11}</option>
+                  <option value={12}>{12}</option>
+                  <option value={13}>{13}</option>
+                  <option value={14}>{14}</option>
+                  <option value={15}>{15}</option>
+                  <option value={16}>{16}</option>
+                  <option value={17}>{17}</option>
+                  <option value={18}>{18}</option>
+                  <option value={19}>{19}</option>
+                  <option value={20}>{20}</option>
+                </select>
+              </form>
+            </div>
+            <div className={styles.optionsBox}>
+              <div className={styles.priceBox}>
+                <span>{'$' + test.price.toLocaleString()}</span>
+              </div>
+            </div>
+          </BaseGrid>
+        </BaseGrid>
+      </ContentBox>
+    </Container>
   );
 };
 
-export default productOrderCard;
+export default ProductOrderCard;
