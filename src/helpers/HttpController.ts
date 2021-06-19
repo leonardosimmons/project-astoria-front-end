51c9c19ac2eaad3a481f47da57614e51e888c2b3
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-06-19 05:52:23 -0700

    revamped and cleaned up for auth

--- src/helpers/HttpController.ts
+++ src/helpers/HttpController.ts
@@ -1,26 +1,31 @@
 
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
-import { HttpHeader } from '../utils/types/types';
+import { HttpResponse, HttpServerResponse } from '../utils/types';
 
 
 interface HttpControllerInterface {
   get: (url: string) => Promise<any>;
-  post<T>(url: string, data: T): Promise<any>;
-  put<T>(url: string, data: T): Promise<any>;
-  signInUser: (u_id: number, header: HttpHeader) => Promise<any>;
+  post<T>(url: string, data: T, token: string): Promise<AxiosResponse<any> | false>;
+  put<T>(url: string, data: T): Promise<AxiosResponse<any> | false>;
+  signInUser: (u_id: number) => Promise<any>;
 };
 
 
 class HttpController implements HttpControllerInterface
 {
   private _buffer: any;
+  private _auth: string;
   private _conn: AxiosInstance;
 
-  constructor(baseUrl?: string, headers?: any) {
+  constructor() {
     this._buffer;
+    this._auth = JSON.parse(localStorage.getItem('auth-token') as string);
     this._conn = axios.create({
-      baseURL: baseUrl || process.env.NEXT_PUBLIC_BASE_API as string,
-      headers: headers || { 'Content-Type': 'application/json' }
+      baseURL: process.env.NEXT_PUBLIC_BASE_API as string,
+      headers: {
+        post: { 'auth-token': `Bearer ${this._auth}`},
+        put: { 'auth-token': `Bearer ${this._auth}`}
+      }
     });
   };
 
@@ -28,79 +33,59 @@ class HttpController implements HttpControllerInterface
     return this._buffer;
   };
 
-  public async get(url: string, headers?: HttpHeader): Promise<any> {
+  public async get(url: string): Promise<any> {
     try {
-      return await this._conn({ 
-        method: 'get',
-        headers: headers 
-        ? {...headers} 
-        : {'Content-Type': 'application/json'},  
-        url 
-      })
-      .then((res: AxiosResponse<any>) => res.status === 200 && res.data.payload)
-      .then((data: any) => {
-        this._buffer = data;
-        return data;
-      });
+      const res: AxiosResponse<any> = await this._conn({ method: 'get', url });
+      const data: HttpResponse = res.status === 200 && res.data;
+
+      this._buffer = data.payload;
+      return data.payload;
     }
     catch(err) {
       console.log(err);
-      return false;
+      throw new Error(err);
     }
   };
 
-  public async post<T>(url: string, data: T, headers?: HttpHeader): Promise<AxiosResponse<any> | false> {
+  public async post<T>(url: string, data: T): Promise<AxiosResponse<any> | false> {
     try {
-      return await this._conn({
-        method:'post',
-        headers: headers 
-        ? {...headers} 
-        : {'Content-Type': 'application/json'}, 
-        url, 
-        data: {...data} 
-      })
-      .then((res: AxiosResponse<any>) => res.status === 200 ? res : res.status === 201 ? res : false)
-      .then((data: any) => {
-        this._buffer = data;
-        return data;
-      });
+      const res: AxiosResponse<any> = await this._conn({ method:'post', url, data: {...data} });
+      const response: HttpServerResponse = res.status === 200 ? res : res.status === 201 ? res : false;
+      
+      this._buffer = response;
+      return response;
     }
     catch(err) {
       console.log(err);
-      return false;
+      throw new Error(err);
     }
   };
 
-  public async put<T>(url: string, data: T, headers?: HttpHeader): Promise<AxiosResponse<any> | false> {
+  public async put<T>(url: string, data: T): Promise<AxiosResponse<any> | false> {
     try {
-      return await this._conn({
-        method: 'put',
-        headers: headers 
-        ? {...headers} 
-        : {'Content-Type': 'application/json'},  
-        url, 
-        data
-      })
-      .then((res: AxiosResponse<any>) => res.status === 200 ? res : false)
-      .then((data: any) => {
-        this._buffer = data;
-        return data;
-      });
+      const res: AxiosResponse<any> = await this._conn({ method: 'put', url, data });
+      const response: HttpServerResponse = res.status === 200 ? res : false;
+      
+      this._buffer = response;
+      return response;
     }
     catch(err) {
       console.log(err);
-      return false;
+      throw new Error(err);
     }
   }
 
   public async signInUser(u_id: number): Promise<any> {
     try {
       const api: string = '/sign-in';
-      return await this.put(process.env.NEXT_PUBLIC_USER_API as string + api, {u_id}).then((res) => {
-        if (res) {
-          return res.status === 200 ? res.data.payload : false;
-        }
-      });
+      const res: HttpServerResponse = await this.put(process.env.NEXT_PUBLIC_USER_API as string + api, {u_id});
+      
+      if (res) {
+        const response: HttpResponse = res.status === 200 ? res.data : false;
+
+        this._buffer = response.payload;
+        return response.payload;
+      }
     }
     catch(err) {
       console.log(err);
