fd8b025ab5fdc5d855ecf6d3c61a7fbac28cdb41
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-06-20 15:07:41 -0700

    updated and cleaned up

--- src/helpers/hooks/useUser.ts
+++ src/helpers/hooks/useUser.ts
@@ -4,46 +4,28 @@ import { signOut as signOutSession } from 'next-auth/client';
 import { UserInfo, UserContext, UserData } from '../../utils/types';
 
 import { rand } from '../functions';
-import { useCart } from './useCart';
 
 import { 
   createAndSignInNewUser, 
-  setUser, 
   userSignOut, 
   verifyAndSignInUser 
 } from '../../redux-store/user/actions';
 
 
-const guest: UserContext = {
-  id: rand(100000000, 999999999),
-  info: {
-    name: 'guest', 
-    email: '', 
-    image: ''
-  },
-  status: {
-    isError: false,
-    isSignedIn: false
-  }
-};
-
-
 export function useUser() {
-  const cart = useCart();
   const dispatch = useAppDispatch();
   const user: UserContext = useAppSelector((state) => state.user);
 
   function guestSignIn(): void {
     const token: UserData = {
-      id: guest.id,
+      id: rand(100000000, 999999999),
       info: {
-        ...guest.info,
-        name: `guest_${rand(100000, 999999)}`
+        name: `guest_${rand(100000, 999999)}`,
+        email: '', 
+        image: ''
       }
     };
     
-    cart.assignUser({...token, status: { isError: false, isSignedIn: true }});
-    dispatch(setUser(token));
     dispatch(createAndSignInNewUser(token.info));
   };
   
