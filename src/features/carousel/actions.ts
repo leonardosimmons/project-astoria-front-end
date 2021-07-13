de3502045937c035d39a6b01e483feacec9fce81
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-13 02:24:54 -0700

    fixed type error

--- src/features/carousel/actions.ts
+++ src/features/carousel/actions.ts
@@ -1,14 +1,14 @@
 
-import { AppActions } from '../../redux-store/action-types';
-import { NEXT, PREV, FIRST_SLIDE, LAST_SLIDE, SET_WIDTH, SET_SLIDE_COUNT, SET_DOT_COUNT, RESET_POSITION } from './action-types';
 
-export const next = ():AppActions => ({ type: NEXT });
-export const prev = ():AppActions => ({ type: PREV });
-export const firstSlide = ():AppActions => ({ type: FIRST_SLIDE });
-export const lastSlide = ():AppActions => ({ type: LAST_SLIDE });
-export const resetPositon = ():AppActions => ({ type: RESET_POSITION });
+import { NEXT, PREV, FIRST_SLIDE, LAST_SLIDE, SET_WIDTH, SET_SLIDE_COUNT, SET_DOT_COUNT, RESET_POSITION, CarouselActions } from './action-types';
 
-export const setWidth = (width: number):AppActions => 
+export const next = (): CarouselActions => ({ type: NEXT });
+export const prev = (): CarouselActions => ({ type: PREV });
+export const firstSlide = (): CarouselActions => ({ type: FIRST_SLIDE });
+export const lastSlide = (): CarouselActions => ({ type: LAST_SLIDE });
+export const resetPositon = (): CarouselActions => ({ type: RESET_POSITION });
+
+export const setWidth = (width: number): CarouselActions => 
 (
   {
     type: SET_WIDTH,
@@ -18,7 +18,7 @@ export const setWidth = (width: number):AppActions =>
   }
 );
 
-export const setSlideCount = (count: number):AppActions => 
+export const setSlideCount = (count: number): CarouselActions => 
 (
   {
     type: SET_SLIDE_COUNT,
@@ -28,7 +28,7 @@ export const setSlideCount = (count: number):AppActions =>
   }
 );
 
-export const setDotCount = (count: number[]):AppActions =>
+export const setDotCount = (count: number[]): CarouselActions =>
 (
   {
     type: SET_DOT_COUNT,
