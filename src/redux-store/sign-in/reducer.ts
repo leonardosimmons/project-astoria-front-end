6c57a86d0ada9c9918e70e05c3d6ec9a32b29001
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-17 23:43:22 -0700

    type error fixed

--- src/redux-store/sign-in/reducer.ts
+++ src/redux-store/sign-in/reducer.ts
@@ -1,6 +1,6 @@
 
 import { SignInToken } from "../../utils/types/types";
-import { SET_EMAIL, SET_PASSWORD, SET_VERIFICATION, SignInFormActions } from "./action-type";
+import { SET_SIGN_IN_EMAIL, SET_SIGN_IN_PASSWORD, SET_SIGN_IN_VERIFICATION, SignInFormActions } from "./action-type";
 
 
 export const signInFormInitalState: SignInToken = {
@@ -12,11 +12,11 @@ export const signInFormInitalState: SignInToken = {
 
 export function signInFormReducer(state = signInFormInitalState, action: SignInFormActions): SignInToken {
   switch (action.type) {
-    case SET_EMAIL:
+    case SET_SIGN_IN_EMAIL:
       return {...state, email: action.payload};
-    case SET_PASSWORD:
+    case SET_SIGN_IN_PASSWORD:
       return {...state, password: action.payload};
-    case SET_VERIFICATION:
+    case SET_SIGN_IN_VERIFICATION:
       return {...state, isVerified: action.payload};
     default:
       return state;
