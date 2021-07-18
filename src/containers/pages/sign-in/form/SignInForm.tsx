734a5a7b958b72f418f4477cf34e7df7faa2e20a
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-17 23:18:40 -0700

    connected to backend api

--- src/containers/pages/sign-in/form/SignInForm.tsx
+++ src/containers/pages/sign-in/form/SignInForm.tsx
@@ -2,21 +2,22 @@
 import React from 'react';
 import { SignInForm } from '../../../../utils/types';
 
+import styles from './SignInForm.module.scss';
+
 import Input from '../../../../components/input';
 
-type Props = SignInForm & {
-  styles: any;
-};
+
+type Props = SignInForm;
 
 
-const signInForm: React.FunctionComponent<Props> = ({ email, password, submit, styles }): JSX.Element => {
+const signInForm: React.FunctionComponent<Props> = ({ email, password, submit }): JSX.Element => {
   return (
-    <form onSubmit={ submit }>
-      <Input col labelFront={'Email Address'} styles={ styles } changed={ email }/>
-      <Input col type={'password'} labelFront={'Password'} styles={ styles } changed={ password }/>
+    <form id={'sign-in-form'} onSubmit={submit}>
+      <Input col type={'email'} labelFront={'Email Address'} styles={styles} changed={ email }/>
+      <Input col type={'password'} labelFront={'Password'} styles={styles} changed={ password }/>
       <div className={`${ styles.btnContainer } relative center`}>
-        <Input type={'submit'} value={'SUBMIT'} classes={'relative btn-hoverConfig btn-activeFocus'}/>
-        <Input type={'reset'} value={'RESET'} classes={'relative btn-hoverConfig btn-activeFocus'}/>
+        <Input type={'reset'} value={'RESET'} classes={'relative btn-default btn-hoverConfig btn-activeFocus'}/>
+        <Input type={'submit'} value={'SUBMIT'} classes={'relative btn-default btn-hoverConfig btn-activeFocus'}/>
       </div>
     </form>
   );
--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -2,10 +2,15 @@
 import React from 'react';
 import { NextRouter, useRouter } from 'next/router';
 import { signIn, useSession } from 'next-auth/client';
+import { useAppDispatch, useAppSelector } from '../helpers/hooks/redux';
+import { setEmail, setPassword, verifyUserSignIn } from '../redux-store/sign-in/actions';
+import { SignInToken } from '../utils/types/types';
 import { page } from '../utils/keys';
 
 import styles from '../containers/pages/sign-in/SignIn.module.scss';
 import { useWatchUserSignIn } from '../helpers/hooks/useWatchUserSignIn';
+import { handleInputRef, preventDefault } from '../helpers/functions/functions';
+import { ValidationController } from '../helpers/ValidationController';
 import { useUser } from '../helpers/hooks/useUser';
 
 import Layout from '../containers/layout';
@@ -13,12 +18,17 @@ import Container from '../components/container';
 import ContentBox from '../components/box';
 import TextBox from '../components/text';
 import Copyright from '../components/copyright';
+import SignInForm from '../containers/pages/sign-in/form/SignInForm';
 
 
 function signInPage(): JSX.Element {
-  const [ session ] = useSession();
-  const router: NextRouter = useRouter();
   const user = useUser();
+  const [ session ] = useSession();
+  const dispatch = useAppDispatch();
+  const router: NextRouter = useRouter();
+  const token: SignInToken = useAppSelector((state) => state.sign_in);
+  const validate: ValidationController = new ValidationController();
+  const [ completed, setCompleted ] = React.useState<boolean>(false);
 
   // redirects if user is already logged in
   if(session) {
@@ -37,6 +47,35 @@ function signInPage(): JSX.Element {
     signIn(provider, {callbackUrl: url});
   };
 
+  const emailRef = React.useRef<string>('');
+  const passwordRef = React.useRef<string>('');
+
+  const handleEmail = React.useCallback(handleInputRef(emailRef), []);
+  const handlePassword = React.useCallback(handleInputRef(passwordRef), []);
+  const handleSubmit = React.useCallback(preventDefault(() => {
+    validate.signInForm(emailRef.current, passwordRef.current);
+
+    if (!validate.isValidated) {
+      alert(validate.error);
+      return;
+    }
+    
+    dispatch(setEmail(emailRef.current));
+    dispatch(setPassword(passwordRef.current));
+    setCompleted(true);
+  }), []);
+
+  // dispatches thunk to verify entered creditials server side
+  React.useEffect(() => {
+    if (completed) {
+      dispatch(verifyUserSignIn({
+        email: emailRef.current,
+        password: passwordRef.current,
+        isVerified: false
+      }));
+    }
+  }, [completed]);
+
   return (
     <Layout
       solid
@@ -49,7 +88,12 @@ function signInPage(): JSX.Element {
       <Container wrapper styles={ styles } classes={'relative center-start noselect'}>
         <Container main styles={ styles } classes={'relative center-col'}>
           <ContentBox styles={ styles } classes={'center-col-start'}>
-            <TextBox mainHeading={'WELCOME'} styles={ styles }/>
+            <TextBox mainHeading={'WELCOME'} headingOne={'Sign In'} styles={ styles }/>
+            <SignInForm 
+              email={handleEmail}
+              password={handlePassword}
+              submit={handleSubmit}
+            />
             <button className={'btn-activeFocus'} onClick={() => userSignIn('github', '/')}>
               {'Sign in with GitHub'}
             </button>
