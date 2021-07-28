6224f2167ad619fccc5971d54ac2d38e78118111
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-07-28 04:15:17 -0700

    added scss module

--- src/features/stripe/components/PaymentForm.tsx
+++ src/features/stripe/components/PaymentForm.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { CardElement } from '@stripe/react-stripe-js';
 import { AmountToken, CartContext, ProductCartToken } from '../../../utils/types';
 
+import styles from '../PaymentForm.module.scss';
 
 import { useStripeController } from '../hooks/useStripeController';
 import { formatAmount } from '../../../helpers/functions';
@@ -16,7 +17,7 @@ const testToken: AmountToken = {
 };
 
 const StripeCheckout: React.FunctionComponent = (): JSX.Element => { 
-  const cart: CartContext = useAppSelector((state) => state.cart) 
+  const cart: CartContext = useAppSelector((state) => state.cart);
   const { errorCheck, form, status, submit } = useStripeController();
 
   function formatTotal() {
@@ -29,22 +30,22 @@ const StripeCheckout: React.FunctionComponent = (): JSX.Element => {
   }
 
   return (
-    <div >
-      <div >
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
         <form 
           id="payment-form" 
-       
+          className={styles.form} 
           onSubmit={submit}
           ref={form.styles.wrapper}
         >
-          <div >
-            <div id="card-element" >
+          <div className={styles.inputRow}>
+            <div id="card-element" className={`${styles.input} ${styles.cardElement}`}>
               <CardElement onChange={errorCheck}/>
             </div>
           </div>
           <div 
             id="card-errors" 
-        
+            className={styles.error} 
             role="alert"
             ref={form.styles.error}>
           { status.error 
@@ -55,11 +56,11 @@ const StripeCheckout: React.FunctionComponent = (): JSX.Element => {
           <button 
             id="submit" 
             ref={form.styles.button}
-        
+            className={styles.button}
             disabled={status.disabled || status.processing || status.succeeded}>
             <div 
               id="spinner" 
-        
+              className={`${styles.spinner} hidden`}
               ref={form.styles.spinner}/>
             {
               !status.succeeded && !status.processing
@@ -75,7 +76,7 @@ const StripeCheckout: React.FunctionComponent = (): JSX.Element => {
             }
           </button>
         </form>
-        <div ref={form.styles.result}>
+        <div className={`${styles.result} hidden`} ref={form.styles.result}>
           <p style={{textAlign: 'center', fontWeight: 'bold'}}>{'Payment completed'}<br /></p>
           <pre ref={form.styles.pre}>
             <iframe src="https://giphy.com/embed/l41lS0IgRIFkAuA5G" width="280" height="280" frameBorder="0" className="giphy-embed" style={{margin: '0 auto'}} allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dancing-friday-weekend-l41lS0IgRIFkAuA5G"></a></p>
